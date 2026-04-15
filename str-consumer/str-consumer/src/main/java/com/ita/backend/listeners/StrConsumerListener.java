7bd7441b062549f1e4e2fe3690ed373ebb529e5c
Ismael <ismaelgarciacamacho38@gmail.com>
2026-04-15 12:50:56 +0200

    readme

--- str-consumer/str-consumer/src/main/java/com/ita/backend/listeners/StrConsumerListener.java
+++ str-consumer/str-consumer/src/main/java/com/ita/backend/listeners/StrConsumerListener.java
@@ -1,5 +1,7 @@
 package com.ita.backend.listeners;
 
+import com.ita.backend.application.KafkaMessageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -7,25 +9,33 @@ import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
+@RequiredArgsConstructor
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = @TopicPartition(topic = "str-topic",partitions = {"0"})
-            ,containerFactory = "validMessageContainerFactory")
-    public void listener1(String message){
-        log.info("LISTENER1 ::: Recibiendo un mensaje {}",message);
-    }
+    private final KafkaMessageService kafkaMessageService;
 
     @KafkaListener(groupId = "group-1",
-            topicPartitions = @TopicPartition(topic = "str-topic",partitions = {"1"})
-            ,containerFactory = "validMessageContainerFactory")
-    public void listener2(String message){
-        log.info("LISTENER2 ::: Recibiendo un mensaje {}",message);
+            topicPartitions = @TopicPartition(topic = "str-topic", partitions = {"0"}),
+            containerFactory = "validMessageContainerFactory")
+    public void listener1(String message) {
+        log.info("LISTENER1 ::: Recibiendo un mensaje {}", message);
+        kafkaMessageService.saveMessage(message);
+        log.info("LISTENER1 ::: Mensaje persistido en base de datos");
     }
 
-    @KafkaListener(groupId = "group-2",topics = "str-topic",containerFactory = "validMessageContainerFactory")
-    public void listener3(String message){
-        log.info("LISTENER3 ::: Recibiendo un mensaje {}",message);
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = @TopicPartition(topic = "str-topic", partitions = {"1"}),
+            containerFactory = "validMessageContainerFactory")
+    public void listener2(String message) {
+        log.info("LISTENER2 ::: Recibiendo un mensaje {}", message);
+        kafkaMessageService.saveMessage(message);
+        log.info("LISTENER2 ::: Mensaje persistido en base de datos");
     }
 
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
+    public void listener3(String message) {
+        log.info("LISTENER3 ::: Recibiendo un mensaje {}", message);
+        kafkaMessageService.saveMessage(message);
+        log.info("LISTENER3 ::: Mensaje persistido en base de datos");
+    }
 }
